32ec4cd33f54702bd76452ededb463e7fd28c4f5
Haosen Tan <htan68@uwo.ca>
2022-09-21 15:32:06 -0400

    feat(whole project):add Add search by name javascript

--- static/app.js
+++ static/app.js
@@ -1,26 +1,68 @@
 
-// var Bulbasau = {
-//     name: "Bulbasau",
-//     id: 01,
-//     type: "Grass",
-//     EvolutionStage: 1
-// }
-// var Ivysaur = {
-//     name:Ivysaur,id:02,type:Grass,EvolutionStage:2
-// }
-var Parry=[
+const Parry=[
     {name:"Bulbasau",id:01,type:"Grass",EvolutionStage:1},
     {name:"Ivysaur",id:02,type:"Grass",EvolutionStage:2}
 ]
-var text;
-function myFunction() {
-        let text = document.getElementById("myInput").value;
-        // alert("文本为: " + text);
-        return text;
+
+
+// add event listener
+const textSearchBtn = document.getElementById("text-search-btn");
+textSearchBtn.addEventListener("click", textSearchClickListener);
+const numberSearchBtn = document.getElementById("number-search-btn");
+
+function validNumberInput(inputNumber) {
+    // TODO: validate user's input number
+    return true;
 }
-for(var i=0;i<2;i++){
-    if(Parry[i].name == text)
-    // return Parry[i].id;
-    console.log(Parry[1]);
-    // console.log(Parry[1].name)
+
+function searchByNumber(number, Parry) {
+    let result = [];
+    for (let i = 0; i < Parry.length; i++) {
+        if (Parry[i].id === number) {
+            result.push(Parry[i]);
+        }
+    }
+    return result;
+}
+
+function numberSearchClickListener() {
+    let text = document.getElementById("number-input").value;
+
+    // validate user's input
+    if (!validNumberInput(text)) {
+        return;
+    }
+
+    // search for the text in the array
+    let num = Number(text);
+    let result = searchByNumber(num, Parry);
+    console.log(result);
+
+    // display searching result
+    let resultDiv = document.getElementById("number-search-result");
+    resultDiv.innerHTML = "";
+    for (let i = 0; i < result.length; i++) {
+        resultDiv.innerHTML += "Name: " + result[i].name + " Id: " + result[i].id + result[i].type + result[i].EvolutionStage + "<br>";
+    }
+}
+
+numberSearchBtn.addEventListener("click", numberSearchClickListener);
+
+function searchByText(text, Parry) {
+    let result = [];
+    for (let i = 0; i < 2; i++) {
+        if (Parry[i].name === text) {
+            result.push(Parry[i]);
+        }
+    }
+    return result;
+}
+
+
+function validTextInput(inputText) {
+    if (inputText === "") {
+        alert("请输入内容");
+        return false;
+    }
+    return true;
 }
